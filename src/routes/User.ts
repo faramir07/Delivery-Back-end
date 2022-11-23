f8ba0becc307694361bcc1d7d5494e35f98df412
miguel linares <97300150+faramir07@users.noreply.github.com>
2022-11-23 12:44:14 -0500

    init proyect

---
+++ src/routes/User.ts
@@ -0,0 +1,9 @@
+import { Router } from "express";
+
+const router = Router();
+
+router.get('/', async (_req, res) =>{
+  res.status(200).json('hola')
+})
+
+export default router
---
+++ src/routes/index.ts
@@ -0,0 +1,10 @@
+import { Router } from 'express';
+require('dotenv').config()
+import User from './User'
+
+const routes = Router();
+
+// ejemplo
+routes.use('/user', User)
+
+export default routes;
