722f7cc185ad1283162bf22ceb1483b411f7bbd0
miguel linares <97300150+faramir07@users.noreply.github.com>
2022-11-28 17:53:43 -0500

    add model imageEvidences and StateServides

---
+++ models/ImageEvidences.ts
@@ -0,0 +1,66 @@
+"user strict";
+
+import { Model, UUIDV4 } from "sequelize";
+
+type ImagetypeType = "evidence" | "document";
+
+interface ImageEvidencesType {
+  id: string;
+  imagename: string;
+  image: string;
+  imagetype: ImagetypeType;
+  addressimage: string;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+module.exports = (sequelize: any, DataTypes: any) => {
+  class ImageEvidences
+  extends Model<ImageEvidencesType>
+  implements ImageEvidencesType
+  {
+    id!: string;
+    imagename!: string;
+    image!: string;
+    imagetype!: ImagetypeType;
+    addressimage!: string;
+    createdAt!: Date;
+    updatedAt!: Date;
+
+    // static associate(models: any) {
+    // define association here
+    // }
+  }
+
+  ImageEvidences.init(
+    {
+      id: {
+        type: DataTypes.UUID,
+        defaultValue:UUIDV4,
+        allowNull: false,
+        primaryKey: true
+      },
+      imagename: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      image: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      imagetype: {
+        type: DataTypes.UNUM,
+        allowNull: false,
+        values: ["evidence", "document"],
+      },
+      addressimage: DataTypes.STRING,
+      createdAt: DataTypes.DATE,
+      updatedAt: DataTypes.DATE,
+    },
+    {
+      sequelize,
+      modelName: "ImageEvidences"
+    }
+  );
+  return ImageEvidences;
+}
---
+++ models/StateServices.ts
@@ -0,0 +1,68 @@
+"user strict";
+import { Model, UUIDV4 } from "sequelize";
+
+interface StateServicesType {
+  id: string;
+  checkinput: boolean;
+  checkoutput: boolean;
+  evidence: boolean;
+  address: string;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+module.exports = (sequelize: any, DataTypes: any) => {
+  class StateServices
+    extends Model<StateServicesType>
+    implements StateServicesType
+  {
+    id!: string;
+    checkinput!: boolean;
+    checkoutput!: boolean;
+    evidence!: boolean;
+    address!: string;
+    createdAt!: Date;
+    updatedAt!: Date;
+
+    // static associate(models: any) {
+    // define association here
+    // }
+  }
+
+  StateServices.init(
+    {
+      id: {
+        type: DataTypes.UUID,
+        defaultValue: UUIDV4,
+        allowNull: false,
+        primaryKey: true,
+      },
+      checkinput: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+        defaultValue: false,
+      },
+      checkoutput: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+        defaultValue: false,
+      },
+      evidence: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+        defaultValue: false,
+      },
+      address: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      createdAt: DataTypes.DATE,
+      updatedAt: DataTypes.DATE,
+    },
+    {
+      sequelize,
+      modelName: "StateServices",
+    }
+  );
+  return StateServices;
+};
