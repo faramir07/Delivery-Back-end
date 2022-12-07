0e83b7fc5cf8c9b058a21ce7064c812a7f654390
miguel linares <97300150+faramir07@users.noreply.github.com>
2022-12-07 11:14:58 -0500

    add register delivery

--- src/routes/Registration.ts
+++ src/routes/Registration.ts
@@ -1,15 +1,66 @@
 import { Router } from "express";
-import { registration } from "../controller/registrationClient"
+import {
+  registrationClient,
+  registrationDelivery,
+} from "../controller/registration";
 
 const router = Router();
 
-router.post("/cliente", async (req, res) => {
-  const { firstname, lastname, email, password, address, age, ci, phome } = req.body;
+// ruta registro de un nuevo cliente --------------------------------
+router.post("/client", async (req, res, next) => {
+  const { firstname, lastname, email, password, address, age, ci, phome } =
+    req.body;
   try {
-    const controllerUser = await registration(firstname, lastname, email, password, address, age, ci, phome);
+    const controllerUser = await registrationClient(
+      firstname,
+      lastname,
+      email,
+      password,
+      address,
+      age,
+      ci,
+      phome
+    );
     res.json(controllerUser);
   } catch (error: any) {
-    res.status(500).json({error: error.message})
+    error = { status: 400, error: error.message };
+    next(error);
+  }
+});
+
+// ruta registro de un nuevo delivery --------------------------------
+router.post("/delivery", async (req, res, next) => {
+  const {
+    firstname,
+    lastname,
+    email,
+    password,
+    address,
+    age,
+    ci,
+    phome,
+    imagename,
+    image,
+    imagetype,
+  } = req.body;
+  try {
+    const controllerDelivery = await registrationDelivery(
+      firstname,
+      lastname,
+      email,
+      password,
+      address,
+      age,
+      ci,
+      phome,
+      imagename,
+      image,
+      imagetype
+    );
+    res.json(controllerDelivery);
+  } catch (error: any) {
+    error = { status: 400, error: error.message };
+    next(error);
   }
 });
 
