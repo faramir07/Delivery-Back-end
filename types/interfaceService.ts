064800c0f7c4ba28754724c49b6329881316801f
miguel linares <97300150+faramir07@users.noreply.github.com>
2023-01-15 17:18:56 -0500

    add interface delivery service

--- types/interfaceService.ts
+++ types/interfaceService.ts
@@ -1,3 +1,8 @@
+import { DeliveryModelType, DeliveryResService } from "./interfaceDelivery";
+import {
+  StateSericeResType,
+  StateServicesModelType,
+} from "./interfaceStateServices";
 
 // tipo de modelo sql servicio
 export type Statetype = "pending" | "assigned" | "cancelled" | "finished";
@@ -13,20 +18,74 @@ export interface ServiceModelType {
   profit: number;
   createdAt: Date;
   updatedAt: Date;
+  userASer_id?: string | null;
+  userCSer_id?: string | null;
+  userDSer_id?: string | null;
 }
 
 // tipo de put por el admin, moderador y cliente
 export type PoingType = {
   address: string;
   description: string;
-  stateSer_id?: string
-}
+  stateSer_id?: string;
+};
 
 export interface ServicePutType {
   value: number;
   typepayment: TypePaymentType;
   typeservice: TypeServiceType;
+  clientId: string;
+  adminId: string;
   poing: PoingType[];
 }
 
+//tipo de res servicio para asignar
+export interface serviceToAssignType {
+  id: string;
+  value: number;
+  typepayment: TypePaymentType;
+  typeservice: TypeServiceType;
+  state: Statetype;
+  profit: number;
+  createdAt: Date;
+  updatedAt: Date;
+  userASer_id?: string | null;
+  userCSer_id?: string | null;
+  userDSer_id?: string | null;
+  StateServices: StateSericeResType[];
+}
 
+//tipo de sql servicio/estado con delivery
+export interface ServiceStateDeliveryModelType {
+  id: string;
+  value: number;
+  typepayment: TypePaymentType;
+  typeservice: TypeServiceType;
+  state: Statetype;
+  profit: number;
+  createdAt: Date;
+  updatedAt: Date;
+  userASer_id?: string;
+  userCSer_id?: string;
+  userDSer_id?: string;
+  StateServices: StateServicesModelType[];
+  UserDelivery: DeliveryModelType;
+}
+
+//tipo de res servicio/estado con delivery
+
+export interface ServiceStateDeliveryResType {
+  id: string;
+  value: number;
+  typepayment: TypePaymentType;
+  typeservice: TypeServiceType;
+  state: Statetype;
+  profit: number;
+  createdAt: Date;
+  updatedAt: Date;
+  userASer_id?: string | null;
+  userCSer_id?: string | null;
+  userDSer_id?: string | null;
+  StateServices: StateSericeResType[];
+  UserDelivery: DeliveryResService;
+}
